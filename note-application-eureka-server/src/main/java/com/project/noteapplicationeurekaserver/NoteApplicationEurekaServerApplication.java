7c5c9d39ba158ec993df21363105bccea1566dd3
omer <o.f.cavdar10@gmail.com>
2019-10-16 00:49:39 +0300

    adding eureka server

---
+++ note-application-eureka-server/src/main/java/com/project/noteapplicationeurekaserver/NoteApplicationEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.noteapplicationeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApplicationEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteApplicationEurekaServerApplication.class, args);
+	}
+
+}
